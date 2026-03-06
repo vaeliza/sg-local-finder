4a741d6cf7b457be692876b047d092aae13ebb8d
vaeliza <ivahanxuexin@gmail.com>
2026-03-06 23:50:53 +0800

    Update supabase_client.py

--- supabase_client.py
+++ supabase_client.py
@@ -1,1 +1,9 @@
+from supabase import create_client
+import streamlit as st
 
+def get_supabase_client():
+
+    url = st.secrets["SUPABASE_URL"]
+    key = st.secrets["SUPABASE_KEY"]
+
+    return create_client(url, key)
