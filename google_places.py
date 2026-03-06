4dc41b8a1016bb81638ff0d9c8e99800b74212bd
vaeliza <ivahanxuexin@gmail.com>
2026-03-06 23:51:27 +0800

    Update google_places.py

--- google_places.py
+++ google_places.py
@@ -1,1 +1,18 @@
+import requests
+import streamlit as st
 
+API_KEY = st.secrets["GOOGLE_API_KEY"]
+
+def get_place_details(place_id):
+
+    url = "https://maps.googleapis.com/maps/api/place/details/json"
+
+    params = {
+        "place_id": place_id,
+        "key": API_KEY,
+        "fields": "name,rating,user_ratings_total,opening_hours,photos,reviews,website"
+    }
+
+    response = requests.get(url, params=params)
+
+    return response.json()
