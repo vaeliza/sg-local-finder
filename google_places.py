6e28c981beec71a1a055c66b8a70ed19ed5b0adc
vaeliza <ivahanxuexin@gmail.com>
2026-03-07 00:25:52 +0800

    Update google_places.py

--- google_places.py
+++ google_places.py
@@ -8,10 +8,10 @@ def get_place_details(place_id):
     url = "https://maps.googleapis.com/maps/api/place/details/json"
 
     params = {
-        "place_id": place_id,
-        "key": API_KEY,
-        "fields": "name,rating,user_ratings_total,opening_hours,photos,reviews,website"
-    }
+    "place_id": place_id,
+    "key": API_KEY,
+    "fields": "name,rating,user_ratings_total,opening_hours,photos,reviews,website"
+}
 
     response = requests.get(url, params=params)
 
